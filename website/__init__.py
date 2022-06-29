272c97ead5b099aed4514a8aa4ea0d7db683eb48
M0ngi <saidanemongi@gmail.com>
2022-06-30 00:03:44 +0100

    Add API route

--- website/__init__.py
+++ website/__init__.py
@@ -7,6 +7,7 @@ from website.routes import views
 def createApp():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getrandom(25)
+    app.config['API_PROXY'] = "http://0.0.0.0:5000/"
 
     from .routes.views import web
     from .routes.api import api
---
+++ website/routes/api/auth.py
@@ -0,0 +1,17 @@
+from crypt import methods
+from .route import api
+from flask import jsonify, make_response, Blueprint
+
+
+auth = Blueprint('auth', __name__, url_prefix='auth/')
+
+@auth.route('login', methods=['POST'])
+def login():
+    return make_response(jsonify(error="Work in progress"), 206)
+
+
+@auth.route('/signup', methods=['POST'])
+def signup():
+    return make_response(jsonify(error="Work in progress"), 206)
+
+api.register_blueprint(auth)
